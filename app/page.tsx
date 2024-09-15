eb149fdd4ce5117dc0afe69d5cd6538ed847f1ae
CarlosArrieta <carrieta@defontana.com>
2024-09-15 13:54:42 -0300

    change yml

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,15 @@ import type { ChatFunctionSchema } from "../amplify/functions/chat/resource";
 import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/api";
 import outputs from "../amplify_outputs.json";
-Amplify.configure(outputs, {
-  API: {
-    GraphQL: {
-      withCredentials:true
-    }
-  }
-})
+
+Amplify.configure(outputs);
+
+const client = generateClient<ChatFunctionSchema>()
+
 export default function App() {
   async function handleClick(){
     
     console.log(Amplify.getConfig())
-    const client = generateClient<ChatFunctionSchema>()
     client.queries.chatFunction({name: "Carlos"})
   }
   return <main>hola <button onClick={handleClick}>click</button></main>;
